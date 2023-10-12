fd28312cbc69a5ab0423616ce0de9e2865de69c5
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2023-10-12 10:23:50 +0100

    added

---
+++ set_PYTHONPATH.sh
@@ -0,0 +1,2 @@
+export PYTHONPATH=$PYTHONPATH:/Users/sw376/Dropbox/Research/projects/flares_emissiveagn/packages/RELAGN/src 
+export PYTHONPATH=$PYTHONPATH:/Users/sw376/Dropbox/Research/software/heasoft-6.32.1/aarch64-apple-darwin22.2.0/lib/python/
